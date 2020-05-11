cd15acbc5b5406b50f86ab0fb6a43580b825d003
onuki0205 <aizu1260027@gmail.com>
2020-05-12 00:27:15 +0900

    add

---
+++ ex02/ex02_p4.s
@@ -0,0 +1,21 @@
+        .data 
+A:      .word 13    #乗数
+B:      .word 37    #被乗数
+C:      .word 0     #積
+
+D:      .word 16    #計算する分のビット数    
+        .text
+
+main:
+
+
+loop:
+
+loopend1:
+
+loopend2:
+
+
+
+exit:
+        j exit
